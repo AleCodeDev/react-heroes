b1efaf52b61825e452571012d97048993acfa181
AleCodeDev <devcode480@gmail.com>
2024-07-27 16:46:08 -0500

    imagenes actualizadas 9

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -18,7 +18,7 @@ export const HeroCard = ({
     characters,
 }) => {
 
-    const heroImageUrl = `https://alecodedev.github.io/react-heroes/heroes/${id}.jpg`;
+    const heroImageUrl = `./heroes/${id}.jpg`;
 
     // const charactesByHero =  (<p>{ characters }</p>);
 
