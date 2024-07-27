b30d6c42ccf6a746295de3977ae7a9da8dab4f0a
AleCodeDev <devcode480@gmail.com>
2024-07-27 16:38:58 -0500

    imagenes actualizadas 7

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -24,7 +24,7 @@ export const HeroPage = () => {
 
             <div className="col-4">
                 <img
-                    src={`/heroes/${id}.jpg`}
+                    src={`https://AleCodeDev.github.io/react-heroes/heroes/${id}.jpg`}
                     alt={hero.superhero}
                     className="img-thumbnail animate__animated animate__fadeInLeft"
                 />
