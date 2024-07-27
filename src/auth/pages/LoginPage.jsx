253e05815e7e8e6ca6843d7363f7f32bb3056ce5
AleCodeDev <devcode480@gmail.com>
2024-07-27 15:15:20 -0500

    imagenes actualizadas

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -28,7 +28,7 @@ export const LoginPage = () => {
   //   });
   // }
 
-  const urlImageBacjG = `/public/custom/deadpool.jpg`
+  const urlImageBacjG = `/custom/deadpool.jpg`
   return (
     <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh', backgroundImage: `url(${urlImageBacjG})`, height: '100vh' , backgroundRepeat: 'no-repeat', objectFit: 'cover' }}>
             <div className="row p-5 rounded" style={{ width: '35%', backgroundColor: '#61616156' }}>
